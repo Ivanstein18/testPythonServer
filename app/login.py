6a82a4565dd1400c9469ff7c94d8ebf9624cc289
Ivanstein <ivanstein18@yandex.ru>
2022-12-03 21:31:19 +0300

    ?

--- app/login.py
+++ app/login.py
@@ -2,7 +2,7 @@ import psycopg2
 
 
 def checkingUsernameAndPassword(username, password):
-        with psycopg2.connect(dbname="xpeH", user="postgres", password= "1234", host= "127.0.0.1") as conn:
+        with psycopg2.connect(dbname="xpeh", user="postgres", password= "1234", host= "127.0.0.1") as conn:
             with conn.cursor() as cur:
                 try:
                     cur.execute(f"SELECT userName FROM registerUsers WHERE userName = '{username}';")
--- app/registration.py
+++ app/registration.py
@@ -7,7 +7,7 @@ def includingUsernameAndPasswordInBase(username, password):
 
 
 
-    with psycopg2.connect(dbname="xpeH", user="postgres", password= "1234", host= "127.0.0.1") as conn:
+    with psycopg2.connect(dbname="xpeh", user="postgres", password= "1234", host= "127.0.0.1") as conn:
 
         with conn.cursor() as cur:
             cur.execute(f"INSERT INTO registerUsers (userName, hash) VALUES ('{username}', '{password}');")
